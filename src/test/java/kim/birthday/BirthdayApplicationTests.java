67a04b9b9740c735344812fc338927050e1da11e
seongeun308 <kse011010@gmail.com>
2025-06-03 20:59:25 +0900

    initial project

---
+++ src/test/java/kim/birthday/BirthdayApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.birthday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
